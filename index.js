2856f28570ded12329ff7813032f84b3f5f6f1ec
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-06-24 13:28:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function takeANumber(line, newName) {
   line.push(newName);
-  return (`Welcome, ${newName}. You are number ${line[newName]} in line.`)
+  console.log(line[newName]);
+  return (`Welcome, ${newName}. You are number ${line[newName]} in line.`);
 }
